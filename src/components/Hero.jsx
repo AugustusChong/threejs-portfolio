c95f6a863920d2f8243dd593f8db4cc633b5fca3
Augustus Chong <112513956+AugustusChong@users.noreply.github.com>
2023-05-02 20:48:32 -0700

    Removed space next to nbsp

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -17,7 +17,7 @@ const Hero = () => {
             Hi, I&apos;m <span className="text-[#915eff]">Augustus</span>
           </h1>
           <p className={`${styles.heroSubText} mt-2 text-white-100`}>
-            I develop Full Stack web applications,
+            I develop Full Stack web applications,&nbsp;
             <br className="sm:block hidden" />
             bringing ideas to life.
           </p>
