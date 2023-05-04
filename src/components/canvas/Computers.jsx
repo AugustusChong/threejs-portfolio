3a0dee302ce5820132d41c6a9c317a9b6dc10f29
Augustus Chong <112513956+AugustusChong@users.noreply.github.com>
2023-05-03 23:30:18 -0700

    Added and changed to laptop 3D model, initialized Loader component

--- src/components/canvas/Computers.jsx
+++ src/components/canvas/Computers.jsx
@@ -5,7 +5,7 @@ import { OrbitControls, Preload, useGLTF } from "@react-three/drei";
 import CanvasLoader from "../Loader";
 
 const Computers = () => {
-  const computer = useGLTF("./desktop_pc/scene.gltf");
+  const computer = useGLTF("./gaming_laptop/scene.gltf");
   return (
     <mesh>
       <hemisphereLight intensity={0.15} groundColor="black" />
@@ -20,9 +20,9 @@ const Computers = () => {
       />
       <primitive
         object={computer.scene}
-        scale={0.75}
-        position={[0, -3.25, -1.5]}
-        rotation={[-0.01, -0.2, -0.1]}
+        scale={1.5}
+        position={[0, -3.25, 0]}
+        rotation={[0, -0.2, -0.3]}
       />
     </mesh>
   );
