a8cf2b96f37c26161fa1eef5e67986625701898c
Augustus Chong <112513956+AugustusChong@users.noreply.github.com>
2023-05-04 00:47:17 -0700

    Added isMobile state to check if the device is a mobile device, added event listeners and callback function to change isMobile depending on the device, utilized isMobile as a prop in Computers, added prop type check for isMobile state

--- src/components/canvas/Computers.jsx
+++ src/components/canvas/Computers.jsx
@@ -1,11 +1,11 @@
 /* eslint react/no-unknown-property: 0 */
-// import { useEffect, useState } from "react";
-import { Suspense } from "react";
+import { Suspense, useEffect, useState } from "react";
+import PropTypes from "prop-types";
 import { Canvas } from "@react-three/fiber";
 import { OrbitControls, Preload, useGLTF } from "@react-three/drei";
 import CanvasLoader from "../Loader";
 
-const Computers = () => {
+const Computers = ({ isMobile }) => {
   const computer = useGLTF("./gaming_laptop/scene.gltf");
   return (
     <mesh>
@@ -21,8 +21,8 @@ const Computers = () => {
       />
       <primitive
         object={computer.scene}
-        scale={1.5}
-        position={[0, -3.25, 0]}
+        scale={isMobile ? 1 : 1.5}
+        position={isMobile ? [0, -3, 0] : [0, -3.5, 0]}
         rotation={[0, -0.2, -0.3]}
       />
     </mesh>
@@ -30,6 +30,24 @@ const Computers = () => {
 };
 
 const ComputersCanvas = () => {
+  const [isMobile, setIsMobile] = useState(false);
+
+  useEffect(() => {
+    const mediaQuery = window.matchMedia("(max-width: 768px)");
+
+    setIsMobile(mediaQuery.matches);
+
+    const handleMediaQueryChange = (e) => {
+      setIsMobile(e.matches);
+    };
+
+    mediaQuery.addEventListener("change", handleMediaQueryChange);
+
+    return () => {
+      mediaQuery.removeEventListener("change", handleMediaQueryChange);
+    };
+  }, []);
+
   return (
     <Canvas
       frameLoop="demand"
@@ -43,11 +61,15 @@ const ComputersCanvas = () => {
           maxPolarAngle={Math.PI / 2}
           minPolarAngle={Math.PI / 2}
         />
-        <Computers />
+        <Computers isMobile={isMobile} />
       </Suspense>
       <Preload all />
     </Canvas>
   );
+};
+
+Computers.propTypes = {
+  isMobile: PropTypes.bool.isRequired,
 };
 
 export default ComputersCanvas;
