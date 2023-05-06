00b13f6725672f46d0ffab82275ff8b5b6271452
Augustus Chong <112513956+AugustusChong@users.noreply.github.com>
2023-05-06 01:08:02 -0700

    Initialize Ball component

--- src/components/canvas/Ball.jsx
+++ src/components/canvas/Ball.jsx
@@ -1,9 +1,58 @@
-import React from 'react'
+/* eslint react/no-unknown-property: 0 */
+import { Suspense } from "react";
+import PropTypes from "prop-types";
+import { Canvas } from "@react-three/fiber";
+import {
+  Decal,
+  Float,
+  OrbitControls,
+  Preload,
+  useTexture,
+} from "@react-three/drei";
+import CanvasLoader from "../Loader";
 
-const Ball = () => {
+const Ball = (props) => {
+  const [decal] = useTexture([props.imgUrl]);
+
   return (
-    <div>Ball</div>
-  )
-}
+    <Float speed={1.75} rotationIntensity={1} floatIntensity={2}>
+      <ambientLight intensity={0.25} />
+      <directionalLight position={[0, 0, 0.05]} />
+      <mesh castShadow receiveShadow scale={4}>
+        <sphereBufferGeometry args={[0.7, 30, 30]} />
+        <meshStandardMaterial
+          color="#fff8eb"
+          polygonOffset
+          polygonOffsetFactor={-5}
+        />
+        <Decal
+          position={[0, 0, 1]}
+          rotation={[2 * Math.PI, 0, 6.25]}
+          map={decal}
+        />
+      </mesh>
+    </Float>
+  );
+};
 
-export default Ball
+const BallCanvas = ({ icon }) => {
+  return (
+    <Canvas frameLoop="always" gl={{ preserveDrawingBuffer: true }}>
+      <Suspense fallback={<CanvasLoader />}>
+        <OrbitControls enableZoom={false} enablePan={false} />
+        <Ball imgUrl={icon} />
+      </Suspense>
+      <Preload all />
+    </Canvas>
+  );
+};
+
+Ball.propTypes = {
+  imgUrl: PropTypes.node.isRequired,
+};
+
+BallCanvas.propTypes = {
+  icon: PropTypes.node.isRequired,
+};
+
+export default BallCanvas;
