d2ec31adf50c57510549917cf311f9991c35ee40
Augustus Chong <112513956+AugustusChong@users.noreply.github.com>
2023-05-07 03:16:33 -0700

    Modified animated button to start scaled at 75% and then scale to 100% when hovered

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -26,7 +26,7 @@ const Hero = () => {
       <ComputersCanvas />
       <div className="absolute xs:bottom-10 bottom-32 w-full flex justify-center items-center">
         <a href="#about">
-          <div className="w-[35px] h-[64px] rounded-3xl border-4 border-secondary flex justify-center items-start p-2">
+          <div className="w-[35px] h-[64px] rounded-3xl border-4 border-secondary flex justify-center items-start p-2 scale-75 hover:scale-100">
             <motion.dev
               animate={{
                 y: [0, 24, 0],
