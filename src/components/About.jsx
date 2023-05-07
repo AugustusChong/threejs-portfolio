bb7e888fe8e7b6629c0712d17de2544b1ed7f461
Augustus Chong <112513956+AugustusChong@users.noreply.github.com>
2023-05-07 14:22:48 -0700

    Center justified service cards in About component

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -53,7 +53,7 @@ const About = () => {
         user-friendly applications that solve real-world problems. Let&apos;s
         create something amazing together!
       </motion.p>
-      <div className="mt-20 flex flex-wrap gap-10">
+      <div className="mt-20 flex flex-wrap gap-10 justify-center">
         {services.map((service, index) => (
           <ServiceCard key={service.title} index={index} {...service} />
         ))}
