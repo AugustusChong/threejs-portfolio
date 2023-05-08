ee95a779a93065a4a7999e53ba689197ecb32c54
Augustus Chong <112513956+AugustusChong@users.noreply.github.com>
2023-05-07 23:36:03 -0700

    Removed unused import, fixed useFrame hook

--- src/components/canvas/Stars.jsx
+++ src/components/canvas/Stars.jsx
@@ -1,5 +1,5 @@
 /* eslint react/no-unknown-property: 0 */
-import { useState, useRef, Suspense } from "react";
+import { useRef, Suspense } from "react";
 import { Canvas, useFrame } from "@react-three/fiber";
 import { Points, PointMaterial, Preload } from "@react-three/drei";
 import * as random from "maath/random/dist/maath-random.esm";
@@ -8,7 +8,7 @@ const Stars = (props) => {
   const ref = useRef();
   const sphere = random.inSphere(new Float32Array(5000), { radius: 1.2 });
 
-  useFrame((state, delta) => {
+  useFrame((_, delta) => {
     ref.current.rotation.x -= delta / 10;
     ref.current.rotation.y -= delta / 15;
   });
