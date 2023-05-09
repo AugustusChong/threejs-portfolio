55285b1a680f4e2556019ddc60bd7f3a4ef96b95
Augustus Chong <112513956+AugustusChong@users.noreply.github.com>
2023-05-08 23:21:18 -0700

    Changed colors and background image in tailwind config

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,21 +6,21 @@ module.exports = {
   theme: {
     extend: {
       colors: {
-        primary: "#050816",
-        secondary: "#aaa6c3",
-        tertiary: "#151030",
-        "black-100": "#100d25",
-        "black-200": "#090325",
+        primary: "#000814",
+        secondary: "#c0c0c0",
+        tertiary: "#000038",
+        "black-100": "#000023",
+        "black-200": "#000042",
         "white-100": "#f3f3f3",
       },
       boxShadow: {
-        card: "0px 35px 120px -15px #211e35",
+        card: "0px 35px 120px -15px #222267",
       },
       screens: {
         xs: "450px",
       },
       backgroundImage: {
-        "hero-pattern": "url('/src/assets/herobg.png')",
+        "hero-pattern": "url('/src/assets/herobg.jpg')",
       },
     },
   },
