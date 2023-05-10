b299d4d7e3c6d3a57d3448e14320fe4b7ba3b160
Augustus Chong <112513956+AugustusChong@users.noreply.github.com>
2023-05-10 16:37:24 -0700

    Updated 3D models for Computers and Earth component

--- src/components/canvas/Computers.jsx
+++ src/components/canvas/Computers.jsx
@@ -6,7 +6,7 @@ import { OrbitControls, Preload, useGLTF } from "@react-three/drei";
 import CanvasLoader from "../Loader";
 
 const Computers = ({ isMobile }) => {
-  const computer = useGLTF("./gaming_laptop/scene.gltf");
+  const computer = useGLTF("./laptop/scene.gltf");
   return (
     <mesh>
       <hemisphereLight intensity={0.15} groundColor="black" />
