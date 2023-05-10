84f19191b58afc2ac55ca6bcc87969f25e796c8d
Augustus Chong <112513956+AugustusChong@users.noreply.github.com>
2023-05-09 22:02:50 -0700

    Added noopener noreferrer to window.open in Works component

--- src/components/Works.jsx
+++ src/components/Works.jsx
@@ -34,7 +34,7 @@ const ProjectCard = ({
           <div className="absolute inset-0 flex justify-end m-3 card-img_hover">
             <div
               onClick={() => {
-                window.open(source_code_link, "_blank");
+                window.open(source_code_link, "_blank", "noopener noreferrer");
               }}
               className="black-gradient w-10 h-10 rounded-full flex justify-center items-center cursor-pointer hover:scale-110"
             >
