1d64aad334dff7cf6980186b4ec459364cb7f641
Augustus Chong <112513956+AugustusChong@users.noreply.github.com>
2023-05-11 00:19:33 -0700

    Implemented DnaLoader in the App component to show a dna spinner when the page is loading

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,31 @@
+import { useEffect, useState } from "react";
 import { BrowserRouter } from "react-router-dom";
 import {
   About,
   Contact,
   Experience,
-  Feedbacks,
+  // Feedbacks,
   Hero,
   Navbar,
   Tech,
   Works,
   StarsCanvas,
 } from "./components";
+import DnaLoader from "./components/DnaLoader";
 
 const App = () => {
-  return (
+  const [loading, setLoading] = useState(true);
+
+  const handleLoading = () => {
+    setLoading(false);
+  };
+
+  useEffect(() => {
+    window.addEventListener("load", handleLoading);
+    return () => window.removeEventListener("load", handleLoading);
+  }, []);
+
+  return !loading ? (
     <BrowserRouter>
       <div className="relative z-0 bg-primary">
         <div className="bg-hero-pattern bg-cover bg-no-repeat bg-center">
@@ -23,13 +36,15 @@ const App = () => {
         <Experience />
         <Tech />
         <Works />
-        <Feedbacks />
+        {/* <Feedbacks /> */}
         <div className="relative z-0">
           <Contact />
           <StarsCanvas />
         </div>
       </div>
     </BrowserRouter>
+  ) : (
+    <DnaLoader />
   );
 };
 
