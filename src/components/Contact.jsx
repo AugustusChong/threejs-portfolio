a41c184d0fc41bc89db5e1e0045e0e05cd816069
Augustus Chong <112513956+AugustusChong@users.noreply.github.com>
2023-05-11 20:12:17 -0700

    Changed contact form to use react-hook-form and implemented validation with Yup, made various changes to the form

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -1,28 +1,47 @@
 import { useState, useRef } from "react";
 import { motion } from "framer-motion";
 import emailjs from "@emailjs/browser";
+import { useForm } from "react-hook-form";
+import { yupResolver } from "@hookform/resolvers/yup";
+import * as yup from "yup";
 import { styles } from "../styles";
 import { EarthCanvas } from "./canvas";
 import { SectionWrapper } from "../hoc";
 import { slideIn } from "../utils/motion";
 
+const validationSchema = yup
+  .object()
+  .shape({
+    name: yup
+      .string()
+      .required("Name is required")
+      .matches(/^[a-zA-Z ]*$/, "Please enter a valid name")
+      .min(2, "Name must be at least 2 characters"),
+    email: yup.string().email("Email is invalid").required("Email is required"),
+    message: yup.string().required("Message is required"),
+  })
+  .required();
+
 const Contact = () => {
+  const {
+    register,
+    handleSubmit,
+    reset,
+    formState: { errors },
+    // watch,
+  } = useForm({ resolver: yupResolver(validationSchema) });
+
+  // console.log(watch("name"));
+  // console.log(watch("email"));
+  // console.log(watch("message"));
+
   const formRef = useRef();
-  const [form, setForm] = useState({
-    name: "",
-    email: "",
-    message: "",
-  });
   const [loading, setLoading] = useState(false);
 
-  const handleChange = (e) => {
-    const { name, value } = e.target;
+  const inputClasses =
+    "bg-tertiary py-4 px-6 placeholder:select-none text-white rounded-lg outline-none border-none font-medium";
 
-    setForm({ ...form, [name]: value });
-  };
-
-  const handleSubmit = (e) => {
-    e.preventDefault();
+  const onSubmit = (data) => {
     setLoading(true);
 
     emailjs
@@ -30,11 +49,12 @@ const Contact = () => {
         "service_yjq5ipm",
         "template_d8xkzkr",
         {
-          from_name: form.name,
+          from_name: data.name,
           to_name: "Augustus",
-          from_email: form.email,
+          from_email: data.email,
+          reply_to: data.email,
           to_email: "augustus.chong.dev@gmail.com",
-          message: form.message,
+          message: data.message,
         },
         "PDMFFARlxpZJ2tq7K"
       )
@@ -42,11 +62,7 @@ const Contact = () => {
         () => {
           setLoading(false);
           alert("Message sent successfully!");
-          setForm({
-            name: "",
-            email: "",
-            message: "",
-          });
+          reset();
         },
         (error) => {
           setLoading(false);
@@ -66,48 +82,76 @@ const Contact = () => {
         <h3 className={styles.sectionHeadText}>Contact</h3>
         <form
           ref={formRef}
-          onSubmit={handleSubmit}
+          onSubmit={handleSubmit(onSubmit)}
           className="mt-12 flex flex-col gap-8"
         >
           <label className="flex flex-col">
-            <span className="text-white font-medium mb-4">Your Name</span>
+            <div className="flex justify-between">
+              <span className="text-white font-medium mb-4">Name</span>
+              <span className="text-red-600">{errors.name?.message}</span>
+            </div>
             <input
+              {...register("name")}
               type="text"
-              name="name"
-              value={form.name}
-              onChange={handleChange}
-              placeholder="What's your name?"
-              className="bg-tertiary py-4 px-6 placeholder:text-secondary text-white rounded-lg outline-none border-none font-medium"
-            ></input>
+              placeholder={
+                !errors.name ? "Your Name" : "How will I know who you are?"
+              }
+              className={`${inputClasses} ${
+                !errors.name
+                  ? "placeholder:text-secondary"
+                  : " placeholder:text-amber-400"
+              }`}
+              aria-invalid={errors.name ? "true" : "false"}
+            />
           </label>
           <label className="flex flex-col">
-            <span className="text-white font-medium mb-4">Your Email</span>
+            <div className="flex justify-between">
+              <span className="text-white font-medium mb-4">Email</span>
+              <span className="text-red-600">{errors.email?.message}</span>
+            </div>
             <input
+              {...register("email")}
               type="email"
-              name="email"
-              value={form.email}
-              onChange={handleChange}
-              placeholder="What's your email?"
-              className="bg-tertiary py-4 px-6 placeholder:text-secondary text-white rounded-lg outline-none border-none font-medium"
-            ></input>
+              placeholder={
+                !errors.email
+                  ? "Your Email"
+                  : "How am I going to reply back to you?"
+              }
+              className={`${inputClasses} ${
+                !errors.email
+                  ? "placeholder:text-secondary"
+                  : "placeholder:text-amber-400"
+              }`}
+              aria-invalid={errors.email ? "true" : "false"}
+            />
           </label>
           <label className="flex flex-col">
-            <span className="text-white font-medium mb-4">Your Message</span>
+            <div className="flex justify-between">
+              <span className="text-white font-medium mb-4">Message</span>
+              <span className="text-red-600">{errors.message?.message}</span>
+            </div>
             <textarea
+              {...register("message")}
               rows={6}
-              name="message"
-              value={form.message}
-              onChange={handleChange}
-              placeholder="What's your message?"
-              className="bg-tertiary py-4 px-6 placeholder:text-secondary text-white rounded-lg outline-none border-none font-medium"
-            ></textarea>
+              placeholder={
+                !errors.message
+                  ? "Your Message"
+                  : "How will I know what you want to say? I can't read minds you know..."
+              }
+              className={`${inputClasses} ${
+                !errors.message
+                  ? "placeholder:text-secondary"
+                  : "placeholder:text-amber-400"
+              }`}
+              aria-invalid={errors.message ? "true" : "false"}
+            />
           </label>
           <div className="flex justify-center">
             <button
               type="submit"
-              className="bg-tertiary py-3 px-8 outline-none w-fit text-white font-bold shadow-md shadow-primary rounded-xl"
+              className="bg-tertiary py-3 px-8 outline-none w-fit text-white font-bold shadow-md shadow-primary rounded-xl select-none"
             >
-              {loading ? "Sending..." : "Submit"}
+              {!loading ? "Submit" : "Sending..."}
             </button>
           </div>
         </form>
