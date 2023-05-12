022e4de7d97ca7e69d4a15fe656275b15edac7c3
Augustus Chong <112513956+AugustusChong@users.noreply.github.com>
2023-05-11 21:50:52 -0700

    Added select-none class name to prevent text selection

--- src/App.jsx
+++ src/App.jsx
@@ -27,7 +27,7 @@ const App = () => {
 
   return !loading ? (
     <BrowserRouter>
-      <div className="relative z-0 bg-primary">
+      <div className="relative z-0 bg-primary select-none">
         <div className="bg-hero-pattern bg-cover bg-no-repeat bg-center">
           <Navbar />
           <Hero />
