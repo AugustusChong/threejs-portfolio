009a16f18febafd26034eb506fb072ae00d780aa
Augustus Chong <112513956+AugustusChong@users.noreply.github.com>
2023-05-13 20:36:06 -0700

    Reverted back to import for App, lazy load was having issues on mobile devices

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,18 @@
 // eslint-disable-next-line no-unused-vars
 import React, { lazy, Suspense, useEffect, useState } from "react";
 import { BrowserRouter } from "react-router-dom";
+import {
+  About,
+  Contact,
+  Experience,
+  // Feedbacks,
+  Hero,
+  Navbar,
+  Tech,
+  Works,
+  StarsCanvas,
+} from "./components";
 import DnaLoader from "./components/DnaLoader";
-
-const Navbar = lazy(() => import("./components/Navbar"));
-const Hero = lazy(() => import("./components/Hero"));
-const About = lazy(() => import("./components/About"));
-const Experience = lazy(() => import("./components/Experience"));
-const Tech = lazy(() => import("./components/Tech"));
-const Works = lazy(() => import("./components/Works"));
-// const Feedbacks = lazy(() => import("./components/Feedbacks"));
-const Contact = lazy(() => import("./components/Contact"));
-const StarsCanvas = lazy(() => import("./components/canvas/Stars"));
 
 const App = () => {
   const [loading, setLoading] = useState(true);
@@ -28,21 +29,19 @@ const App = () => {
   return !loading ? (
     <BrowserRouter>
       <div className="relative z-0 bg-primary select-none">
-        <Suspense fallback={<DnaLoader />}>
-          <div className="bg-hero-pattern bg-cover bg-no-repeat bg-center">
-            <Navbar />
-            <Hero />
-          </div>
-          <About />
-          <Experience />
-          <Tech />
-          <Works />
-          {/* <Feedbacks /> */}
-          <div className="relative z-0">
-            <Contact />
-            <StarsCanvas />
-          </div>
-        </Suspense>
+        <div className="bg-hero-pattern bg-cover bg-no-repeat bg-center">
+          <Navbar />
+          <Hero />
+        </div>
+        <About />
+        <Experience />
+        <Tech />
+        <Works />
+        {/* <Feedbacks /> */}
+        <div className="relative z-0">
+          <Contact />
+          <StarsCanvas />
+        </div>
       </div>
     </BrowserRouter>
   ) : (
