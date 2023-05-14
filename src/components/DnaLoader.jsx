a310217555917a27e19d29de92e62c0f561d4411
Augustus Chong <112513956+AugustusChong@users.noreply.github.com>
2023-05-14 02:55:42 -0700

    Changed dna loader to accept props

--- src/components/DnaLoader.jsx
+++ src/components/DnaLoader.jsx
@@ -1,11 +1,29 @@
+import PropTypes from "prop-types";
 import { Dna } from "react-loader-spinner";
 
-const DnaLoader = () => {
+const DnaLoader = ({ spinnerProps }) => {
   return (
-    <div className="fixed inset-0 z-50 flex justify-center items-center bg-black opacity-100">
-      <Dna visible={true} height="120" width="120" ariaLabel="dna-loading" />
+    <div className={`${spinnerProps.divClassName}`}>
+      <Dna
+        visible={true}
+        height={spinnerProps.height}
+        width={spinnerProps.width}
+        ariaLabel="dna-loading"
+        wrapperClass={spinnerProps.wrapperClassName}
+        wrapperStyle={spinnerProps.wrapperStyle}
+      />
     </div>
   );
+};
+
+DnaLoader.propTypes = {
+  spinnerProps: PropTypes.shape({
+    height: PropTypes.string.isRequired,
+    width: PropTypes.string.isRequired,
+    divClassName: PropTypes.string.isRequired,
+    wrapperClassName: PropTypes.string,
+    wrapperStyle: PropTypes.object,
+  }).isRequired,
 };
 
 export default DnaLoader;
