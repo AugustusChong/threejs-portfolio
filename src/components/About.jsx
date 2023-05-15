7a07ef21138d31e0725458623958bb1c2db0e2cb
Augustus Chong <112513956+AugustusChong@users.noreply.github.com>
2023-05-14 19:08:57 -0700

    Fixed react tilt component options, and made dotlottie player self closing

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -10,26 +10,17 @@ import { SectionWrapper } from "../hoc";
 
 const ServiceCard = ({ index, title, icon }) => {
   return (
-    <Tilt className="xs:w-[250px] w-full">
+    <Tilt
+      options={{ max: 45, scale: 1, speed: 450 }}
+      className="xs:w-[250px] w-full"
+    >
       <motion.div
         variants={fadeIn("right", "spring", 0.5 * index, 0.75)}
         className="w-full sky-sea-gradient p-[1px] rounded-[20px] shadow-card"
       >
-        <div
-          options={{
-            max: 45,
-            scale: 1,
-            speed: 450,
-          }}
-          className="bg-tertiary rounded-[20px] py-5 px-12 min-h-[280px] flex justify-evenly items-center flex-col"
-        >
+        <div className="bg-tertiary rounded-[20px] py-5 px-12 min-h-[280px] flex justify-evenly items-center flex-col">
           <div className="w-36 h-36 object-contain">
-            <dotLottie-player
-              autoplay
-              loop
-              speed={1}
-              src={icon}
-            ></dotLottie-player>
+            <dotlottie-player autoplay loop speed={1} src={icon} />
           </div>
           <h3 className="text-white text-[20px] font-bold text-center">
             {title}
