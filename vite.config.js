f77e0f4c9780babb4e232b7d9d2f3da747754e20
Augustus Chong <112513956+AugustusChong@users.noreply.github.com>
2023-05-16 11:49:40 -0700

    Changed vite config to ignore chevrotain warning

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,30 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  rollupOptions: {
+    output: {
+      manualChunks(id) {
+        if (id.includes("chevrotain")) {
+          return "chevrotain";
+        }
+      },
+    },
+  },
+  build: {
+    rollupOptions: {
+      onwarn(warning, warn) {
+        if (
+          warning.code === "EVAL" &&
+          warning.id?.includes("to-fast-properties.js")
+        ) {
+          return;
+        }
+        warn(warning);
+      },
+      output: {},
+    },
+  },
+});
