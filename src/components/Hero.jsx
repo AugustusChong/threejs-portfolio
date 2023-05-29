aec03489cfa7e6cefd3c89d7d86e6fa93ed26143
Augustus Chong <112513956+AugustusChong@users.noreply.github.com>
2023-05-28 22:46:14 -0700

    Added aria label to animated link

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -25,7 +25,7 @@ const Hero = () => {
       </div>
       <ComputerCanvas />
       <div className="absolute xs:bottom-10 bottom-32 w-full flex justify-center items-center">
-        <a href="#about">
+        <a href="#about" aria-label="Go to About section">
           <div className="w-[35px] h-[64px] rounded-3xl border-4 border-secondary flex justify-center items-start p-2 scale-75 hover:scale-100">
             <motion.div
               animate={{
